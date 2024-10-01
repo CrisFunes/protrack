dee31f7e18ffce90227b67414ec53df2b237a4d8
funesdemian <funesdemian@gmail.com>
2024-10-01 15:44:56 +0000

    add simple jira get request

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,33 @@
-const express = require('express');
-const dotenv = require('dotenv');
+require('dotenv').config();
 
-dotenv.config();
+const express = require('express');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Jira credentials from .env file
+const jiraBaseUrl = process.env.JIRA_BASE_URL;
+const jiraEmail = process.env.JIRA_EMAIL;
+const jiraToken = process.env.JIRA_API_TOKEN;
+
 app.get('/', (req, res) => {
-  res.send('Hello from ProTrack Server!');
+  res.send('Hello from ProTrack Serverrr!');
+});
+
+app.get('/api/jira/projects', async (req, res) => {
+    try {
+        const response = await axios.get(`${jiraBaseUrl}/rest/api/3/project`, {
+            auth: {
+                username: jiraEmail,
+                password: jiraToken,
+            },
+        });
+        res.json(response.data); // Send response back to frontend
+    } catch (error) {
+        console.error('Error fetching Jira projects:', error.message); // Log detailed error message
+        res.status(500).json({ error: error.message });
+    }
 });
 
 app.listen(port, () => {
